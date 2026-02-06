7e8a37b545986ab207a158f50fa033379443abb0
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-06 14:44:31 +0800

    Create environment variables for the database connection

---
+++ src/config/databaseConfig.ts
@@ -0,0 +1,16 @@
+import { config } from "mssql";
+
+const databaseConfig: config = {
+  user: process.env.DATABASE_USER ?? "",
+  password: process.env.DATABASE_PASSWORD ?? "",
+  server: process.env.DATABASE_SERVER_NAME ?? "",
+  port: Number(process.env.DATABASE_PORT) || 1433,
+  database: process.env.DATABASE_NAME ?? "",
+  options: {
+    encrypt: process.env.DATABASE_ENCRYPT?.toLowerCase() === "true",
+    trustServerCertificate:
+      process.env.DATABASE_TRUST_SERVER_CERTIFICATE?.toLowerCase() === "true",
+  },
+};
+
+export default databaseConfig;
