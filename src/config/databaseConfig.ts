2f76b38615cd6c0d360df8a9f8e3a31a4b0c63da
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-06 14:55:06 +0800

    Change default server name to 'localhost'

--- src/config/databaseConfig.ts
+++ src/config/databaseConfig.ts
@@ -3,7 +3,7 @@ import { config } from "mssql";
 const databaseConfig: config = {
   user: process.env.DATABASE_USER ?? "",
   password: process.env.DATABASE_PASSWORD ?? "",
-  server: process.env.DATABASE_SERVER_NAME ?? "",
+  server: process.env.DATABASE_SERVER_NAME ?? "localhost",
   port: Number(process.env.DATABASE_PORT) || 1433,
   database: process.env.DATABASE_NAME ?? "",
   options: {
