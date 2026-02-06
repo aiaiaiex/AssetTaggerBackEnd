bb246784d70c8edef253ec419a257d854b14b6da
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-06 15:06:54 +0800

    Create a single global pool instance

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
+
 import serverConfig from "./config/serverConfig";
+import pool from "./database";
 
 const app = express();
 
@@ -7,8 +9,17 @@ app.get("/ping", (req, res) => {
   res.json({ msg: "pong!" });
 });
 
-app.listen(serverConfig.port, () => {
-  console.log(
-    `Server listening on http://localhost:${serverConfig.port.toFixed(0)}`,
-  );
-});
+pool
+  .connect()
+  .then((pool) => {
+    app.locals.database = pool;
+
+    app.listen(serverConfig.port, () => {
+      console.log(
+        `Server listening on http://localhost:${serverConfig.port.toFixed(0)}`,
+      );
+    });
+  })
+  .catch((err: unknown) => {
+    console.error("ERROR: ", err);
+  });
