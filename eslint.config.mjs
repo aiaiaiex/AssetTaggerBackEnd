568c203da7c4d8f587611d393ba2c4007cbe5ef2
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-11 11:50:20 +0800

    Override tsconfigRootDir option with "import.meta.dirname"
    See more:
    https://typescript-eslint.io/packages/parser/#tsconfigrootdir
    
    https://nodejs.org/api/esm.html#importmetadirname

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ export default defineConfig(
       parser: tseslint.parser,
       parserOptions: {
         project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname,
       },
     },
   },
