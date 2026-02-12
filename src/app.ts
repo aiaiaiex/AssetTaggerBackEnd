a3f88103c9b100bf7c69d005032c55e66a816462
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-12 15:07:12 +0800

    Use rootRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,13 @@
 import express from "express";
-import z from "zod";
 
 import serverConfig from "./configs/serverConfig";
 import pool from "./database";
-import {
-  ConfigurationInformation,
-  ConfigurationInformationSchema,
-} from "./models/ConfigurationFunction";
+import rootRoutes from "./routes/rootRoutes";
 
 const app = express();
 
-app.get("/ping", async (req, res) => {
-  const jsonRes: {
-    errorMessage?: string;
-    message: string;
-    serverName?: z.infer<
-      typeof ConfigurationInformationSchema.shape.ServerName
-    >;
-  } = { message: "Back-end server is pingable." };
-
-  const { recordset } =
-    await req.app.locals.database.query<ConfigurationInformation>(
-      "SELECT @@SERVERNAME AS 'ServerName'",
-    );
-
-  const result = ConfigurationInformationSchema.pick({ ServerName: true })
-    .array()
-    .length(1)
-    .safeParse(recordset);
-
-  if (!result.success) {
-    res.status(500);
-    jsonRes.message =
-      "Back-end server is pingable BUT Microsoft SQL Server name is NOT available!";
-    jsonRes.errorMessage = z.prettifyError(result.error);
-  } else {
-    jsonRes.serverName = result.data[0].ServerName;
-  }
-
-  res.json(jsonRes);
-});
+// Routes
+app.use("/", rootRoutes);
 
 pool
   .connect()
