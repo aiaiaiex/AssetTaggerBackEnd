dafb89c846fd3cb9568048cabda12a53e4d17f76
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-16 17:41:45 +0800

    Create endUserController with createEndUser function

---
+++ src/controllers/endUserController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import sql from "mssql";
+import z from "zod";
+
+import { ExpressError } from "../middlewares/handleError";
+import { EndUser, EndUserSchema } from "../models/EndUser";
+
+export const createEndUser = async (req: Request, res: Response) => {
+  const reqRes = EndUserSchema.omit({
+    EndUserID: true,
+    EndUserPasswordHash: true,
+  })
+    .required({ EndUserPassword: true })
+    .safeParse(req.body);
+
+  if (!reqRes.success) {
+    throw new ExpressError(z.prettifyError(reqRes.error), 400);
+  }
+
+  const { EndUserName, EndUserPassword } = reqRes.data;
+
+  const { recordset } = await req.app.locals.database
+    .request()
+    .input("EndUserName", sql.NVarChar(50), EndUserName)
+    .input("EndUserPassword", sql.NVarChar(255), EndUserPassword)
+    .execute<EndUser>("usp_RegisterEndUser");
+
+  const databaseRes = EndUserSchema.array().length(1).safeParse(recordset);
+
+  if (!databaseRes.success) {
+    throw new ExpressError(z.prettifyError(databaseRes.error), 500);
+  }
+
+  res.json(databaseRes);
+};
