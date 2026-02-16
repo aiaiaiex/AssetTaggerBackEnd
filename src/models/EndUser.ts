e7e0a4571c8da40192a29f6fc9b5fdd420853e9f
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-16 17:40:29 +0800

    Create type and Zod schema for EndUser

---
+++ src/models/EndUser.ts
@@ -0,0 +1,12 @@
+import z from "zod";
+
+export const EndUserSchema = z.object({
+  EmployeeID: z.uuid().optional(),
+  EndUserID: z.uuid(),
+  EndUserName: z.string().max(50),
+  EndUserPassword: z.string().max(255).optional(),
+  EndUserPasswordHash: z.string().length(32),
+  EndUserRoleID: z.uuid().optional(),
+});
+
+export type EndUser = z.infer<typeof EndUserSchema>;
