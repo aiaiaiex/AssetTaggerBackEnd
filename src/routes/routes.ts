088d105cacdd4244343fc95d7860acdc6329cbba
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-16 17:43:00 +0800

    Use endUserRoutes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
+import endUserRoutes from "./endUserRoutes";
 import rootRoutes from "./rootRoutes";
 
 const routes = Router();
 
 routes.use("/", rootRoutes);
+routes.use("/enduser", endUserRoutes);
 
 export default routes;
