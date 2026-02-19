f3e4b5b77ad07ead38582f8501272ff45b796f61
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-19 16:24:05 +0800

    Use Zod to validate serverConfig

--- src/configs/serverConfig.ts
+++ src/configs/serverConfig.ts
@@ -1,9 +1,21 @@
-interface ServerConfig {
-  port: number;
-}
+import z from "zod";
 
-const serverConfig: ServerConfig = {
-  port: Number(process.env.SERVER_PORT) || 3000,
-};
+const ServerConfigSchema = z.object({
+  // 0 <= port <= 65535
+  // See more:
+  // https://datatracker.ietf.org/doc/html/rfc6335#section-6
+  port: z
+    .string()
+    .transform((x) => {
+      return x.trim().length > 0 ? Number(x) : 3000;
+    })
+    .pipe(z.int().gte(0).lte(65535)),
+});
+
+type ServerConfig = z.infer<typeof ServerConfigSchema>;
+
+const serverConfig: ServerConfig = ServerConfigSchema.parse({
+  port: process.env.SERVER_PORT,
+});
 
 export default serverConfig;
