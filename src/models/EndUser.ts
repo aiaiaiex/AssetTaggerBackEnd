cbf3109142a6760d5d6d3e8967b1ed8dc7589a26
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-19 17:36:06 +0800

    Make EndUserName lowercase only

--- src/models/EndUser.ts
+++ src/models/EndUser.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const EndUserSchema = z.object({
   EmployeeID: z.uuid().optional(),
   EndUserID: z.uuid(),
-  EndUserName: z.string().max(50),
+  EndUserName: z.string().max(50).lowercase(),
   EndUserPassword: z.string().max(255).optional(),
   EndUserPasswordHash: z.string().length(32),
   EndUserRoleID: z.uuid().optional(),
