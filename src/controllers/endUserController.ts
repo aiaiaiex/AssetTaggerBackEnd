b1f98803ae2e8e9bbc23cd86915382d7321e3061
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-20 11:42:06 +0800

    Rename reqRes to input and databaseRes to output for better understandability

--- src/controllers/endUserController.ts
+++ src/controllers/endUserController.ts
@@ -6,19 +6,19 @@ import { ExpressError } from "../middlewares/handleError";
 import { EndUser, EndUserSchema } from "../models/EndUser";
 
 export const createEndUser = async (req: Request, res: Response) => {
-  const reqRes = EndUserSchema.omit({
+  const input = EndUserSchema.omit({
     EndUserID: true,
     EndUserPasswordHash: true,
   })
     .required({ EndUserPassword: true })
     .safeParse(req.body);
 
-  if (!reqRes.success) {
-    throw new ExpressError(z.prettifyError(reqRes.error), 400);
+  if (!input.success) {
+    throw new ExpressError(z.prettifyError(input.error), 400);
   }
 
   const { EmployeeID, EndUserName, EndUserPassword, EndUserRoleID } =
-    reqRes.data;
+    input.data;
 
   const { recordset } = await req.app.locals.database
     .request()
@@ -28,14 +28,14 @@ export const createEndUser = async (req: Request, res: Response) => {
     .input("EmployeeID", sql.UniqueIdentifier, EmployeeID)
     .execute<EndUser>("usp_CreateEndUser");
 
-  const databaseRes = EndUserSchema.omit({ EndUserPasswordHash: true })
+  const output = EndUserSchema.omit({ EndUserPasswordHash: true })
     .array()
     .length(1)
     .safeParse(recordset);
 
-  if (!databaseRes.success) {
-    throw new ExpressError(z.prettifyError(databaseRes.error), 500);
+  if (!output.success) {
+    throw new ExpressError(z.prettifyError(output.error), 500);
   }
 
-  res.json(databaseRes.data[0]);
+  res.json(output.data[0]);
 };
