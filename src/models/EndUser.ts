0d5c2c94bb18013c00dccd3100c9a1f0086cba3a
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-20 11:13:34 +0800

    Make properties nullable instead of optional to truly reflect EndUser table in database

--- src/models/EndUser.ts
+++ src/models/EndUser.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const EndUserSchema = z.object({
-  EmployeeID: z.uuid().optional(),
+  EmployeeID: z.uuid().nullable(),
   EndUserID: z.uuid(),
   EndUserName: z
     .string()
@@ -10,9 +10,9 @@ export const EndUserSchema = z.object({
     .refine((x) => {
       return x.search(/\s/) === -1;
     }, "Should not have whitespace!"),
-  EndUserPassword: z.string().max(255).optional(),
+  EndUserPassword: z.string().max(255).optional(), // Optional because it is not in the database.
   EndUserPasswordHash: z.string().length(32),
-  EndUserRoleID: z.uuid().optional(),
+  EndUserRoleID: z.uuid().nullable(),
 });
 
 export type EndUser = z.infer<typeof EndUserSchema>;
