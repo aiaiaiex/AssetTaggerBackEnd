284cb850b7adb21575ed7b115ec327f825b3e823
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-23 15:49:27 +0800

    Assign updateEndUser function to PATCH /:EndUserID route

--- src/routes/endUserRoutes.ts
+++ src/routes/endUserRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 
-import { createEndUser, deleteEndUser } from "../controllers/endUserController";
+import {
+  createEndUser,
+  deleteEndUser,
+  updateEndUser,
+} from "../controllers/endUserController";
 
 const endUserRoutes = Router();
 
 endUserRoutes.post("/", createEndUser);
+endUserRoutes.patch("/:EndUserID", updateEndUser);
 endUserRoutes.delete("/:EndUserID", deleteEndUser);
 
 export default endUserRoutes;
