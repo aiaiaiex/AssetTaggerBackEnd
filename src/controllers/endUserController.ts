e90daef8bc9963f8958dbb4aeaa6ca92417a2ff0
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-23 15:48:56 +0800

    Create updateEndUser function

--- src/controllers/endUserController.ts
+++ src/controllers/endUserController.ts
@@ -40,6 +40,85 @@ export const createEndUser = async (req: Request, res: Response) => {
   res.json(output.data[0]);
 };
 
+export const updateEndUser = async (req: Request, res: Response) => {
+  const paramsInput = EndUserSchema.pick({
+    EndUserID: true,
+  }).safeParse(req.params);
+
+  if (!paramsInput.success) {
+    throw new ExpressError(z.prettifyError(paramsInput.error), 400);
+  }
+
+  const { EndUserID } = paramsInput.data;
+
+  const bodyInput = EndUserSchema.omit({
+    EndUserID: true,
+    EndUserPassword: true,
+    EndUserPasswordHash: true,
+  })
+    .extend({
+      EndUserName: EndUserSchema.shape.EndUserName.nullable(),
+      NullifyEmployeeID: z.int().min(0).max(1).prefault(0),
+      NullifyEndUserRoleID: z.int().min(0).max(1).prefault(0),
+    })
+    .safeParse(req.body);
+
+  if (!bodyInput.success) {
+    throw new ExpressError(z.prettifyError(bodyInput.error), 400);
+  }
+
+  const {
+    EmployeeID,
+    EndUserName,
+    EndUserRoleID,
+    NullifyEmployeeID,
+    NullifyEndUserRoleID,
+  } = bodyInput.data;
+
+  if (
+    !(
+      EndUserName ||
+      EndUserRoleID ||
+      EmployeeID ||
+      NullifyEndUserRoleID ||
+      NullifyEmployeeID
+    )
+  ) {
+    throw new ExpressError("Cannot update without values!", 400);
+  } else if (
+    (NullifyEndUserRoleID && EndUserRoleID) ||
+    (NullifyEmployeeID && EmployeeID)
+  ) {
+    throw new ExpressError("Cannot nullify with a non-null value!", 400);
+  }
+
+  const { recordset } = await req.app.locals.database
+    .request()
+    .input("EndUserID", sql.UniqueIdentifier, EndUserID)
+    .input("EndUserName", sql.NVarChar(50), EndUserName)
+    .input("EndUserRoleID", sql.UniqueIdentifier, EndUserRoleID)
+    .input("EmployeeID", sql.UniqueIdentifier, EmployeeID)
+    .input("NullifyEndUserRoleID", sql.Bit, NullifyEndUserRoleID)
+    .input("NullifyEmployeeID", sql.Bit, NullifyEmployeeID)
+    .execute<EndUser>("usp_UpdateEndUser");
+
+  const output = EndUserSchema.omit({ EndUserPasswordHash: true })
+    .extend({
+      OldEmployeeID: EndUserSchema.shape.EmployeeID,
+      OldEndUserName: EndUserSchema.shape.EndUserName,
+      OldEndUserRoleID: EndUserSchema.shape.EndUserRoleID,
+    })
+    .array()
+    .max(1)
+    .safeParse(recordset);
+
+  if (!output.success) {
+    throw new ExpressError(z.prettifyError(output.error), 500);
+  }
+
+  res.json(output.data[0]);
+};
+
 export const deleteEndUser = async (req: Request, res: Response) => {
   const input = EndUserSchema.pick({
     EndUserID: true,
