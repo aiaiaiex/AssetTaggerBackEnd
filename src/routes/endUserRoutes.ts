bb0cf5ccf543282eeab1749025964e8f7e9ce60c
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-24 16:01:26 +0800

    Assign readEndUser function to GET /:EndUserID route

--- src/routes/endUserRoutes.ts
+++ src/routes/endUserRoutes.ts
@@ -3,12 +3,14 @@ import { Router } from "express";
 import {
   createEndUser,
   deleteEndUser,
+  readEndUser,
   updateEndUser,
 } from "../controllers/endUserController";
 
 const endUserRoutes = Router();
 
 endUserRoutes.post("/", createEndUser);
+endUserRoutes.get("/:EndUserID", readEndUser);
 endUserRoutes.patch("/:EndUserID", updateEndUser);
 endUserRoutes.delete("/:EndUserID", deleteEndUser);
 
