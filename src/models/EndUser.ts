0b8d91361e9166c4d0079b6b36dfa9a343018cb1
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-09 06:53:25 +0800

    Update EndUser model to reflect EndUser table in database

--- src/models/EndUser.ts
+++ src/models/EndUser.ts
@@ -1,18 +1,17 @@
 import z from "zod";
 
 export const EndUserSchema = z.object({
-  EmployeeID: z.uuid({ version: "v4" }).nullable(),
+  EmployeeID: z.uuid({ version: "v4" }),
   EndUserID: z.uuid({ version: "v4" }),
   EndUserName: z
     .stringFormat("no-whitespace", /^[^\s]*$/, {
       error: "Has whitespace: expected string to NOT have whitespace",
     })
     .min(1)
-    .max(50)
-    .lowercase(),
-  EndUserPassword: z.string().max(255).optional(), // Optional because it is not in the database.
-  EndUserPasswordHash: z.string().length(32),
-  EndUserRoleID: z.uuid({ version: "v4" }).nullable(),
+    .max(4000),
+  EndUserPassword: z.string().max(4000).optional(), // Optional because it is not in the database.
+  EndUserPasswordHash: z.hash("sha512"),
+  EndUserRoleID: z.uuid({ version: "v4" }),
 });
 
 export type EndUser = z.infer<typeof EndUserSchema>;
