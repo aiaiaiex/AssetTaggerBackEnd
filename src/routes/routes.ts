509452916eda609f6cae5aa030286f6ccff6c5ec
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-09 05:50:45 +0800

    Use authenticationRoutes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
+import authenticationRoutes from "./authenticationRoutes";
 import endUserRoutes from "./endUserRoutes";
 import rootRoutes from "./rootRoutes";
 
 const routes = Router();
 
 routes.use("/", rootRoutes);
+routes.use("/authentication", authenticationRoutes);
 routes.use("/enduser", endUserRoutes);
 
 export default routes;
