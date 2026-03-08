0092b3b7bb77d5763b3241e2916111a2c8ce47a2
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-09 05:49:10 +0800

    Create logInEndUser

---
+++ src/controllers/authenticationController.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import sql from "mssql";
+import z from "zod";
+
+import { ExpressError } from "../middlewares/handleError";
+import { EndUser, EndUserSchema } from "../models/EndUser";
+
+export const logInEndUser = async (req: Request, res: Response) => {
+  const input = EndUserSchema.pick({
+    EndUserName: true,
+    EndUserPassword: true,
+  })
+    .required({ EndUserPassword: true })
+    .safeParse(req.body);
+
+  if (!input.success) {
+    throw new ExpressError(z.prettifyError(input.error), 400);
+  }
+
+  const { EndUserName, EndUserPassword } = input.data;
+
+  const { recordset } = await req.app.locals.database
+    .request()
+    .input("EndUserName", sql.NVarChar(4000), EndUserName)
+    .input("EndUserPassword", sql.NVarChar(4000), `${EndUserPassword}salt`)
+    .execute<EndUser>("usp_LoginEndUser");
+
+  const output = EndUserSchema.pick({
+    EndUserID: true,
+  })
+    .array()
+    .length(1)
+    .safeParse(recordset);
+
+  if (!output.success) {
+    throw new ExpressError(z.prettifyError(output.error), 401);
+  }
+
+  const { EndUserID } = output.data[0];
+
+  const token = jwt.sign({ EndUserID: EndUserID }, "secret", {
+    algorithm: "HS256",
+    expiresIn: 60 * 60 * 8,
+  });
+
+  res.cookie("access_token", token, {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 8,
+    sameSite: "strict",
+    secure: false,
+  });
+
+  res.status(200).end();
+};
