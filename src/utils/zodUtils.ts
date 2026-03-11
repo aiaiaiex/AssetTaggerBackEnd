79823a11f58866437dd045754dbe30746d928e77
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-11 11:41:42 +0800

    Update zodParseNumber to only accept number schemas and to not accept prefaultValue anymore
    Because of Zod's xor function which you could input zodParseNumber and other non-number schemas. prefaultValue is removed because you could use Zod's prefault function.
    
    See more:
    https://zod.dev/api#exclusive-unions-xor
    https://zod.dev/api?id=prefaults

--- src/utils/zodUtils.ts
+++ src/utils/zodUtils.ts
@@ -29,25 +29,17 @@ export const zodParseNull = (
     .pipe(z.null());
 };
 
-export function zodParseNumber<
-  T extends z4.$ZodType<null | number | string | undefined>,
->(
+export function zodParseNumber<T extends z4.$ZodType<number>>(
   zodSchema: T,
-  prefaultValue?: number | string,
-  emptyStringValue: number | string | undefined = NaN,
+  emptyStringValue: unknown = NaN,
 ) {
   return z
     .transform((x) => {
-      if (typeof x === "string") {
-        if (x.search(/\s/) === -1) {
-          // x.length > 0 is checked to prevent calling Number() with an empty string (or a string with only whitespace which is impossible in this case) which results to the number 0.
-          // Change emptyStringValue to 0 to revert back to Number()'s default behavior.
-          return x.length > 0 ? Number(x) : emptyStringValue;
-        } else {
-          return x;
-        }
-      } else if (typeof x === "undefined") {
-        return prefaultValue;
+      // Do not try to convert strings with whitespace into numbers.
+      if (typeof x === "string" && x.search(/\s/) === -1) {
+        // x.length > 0 is checked to prevent calling Number() with an empty string (or a string with only whitespace which is impossible in this case) which results to the number 0.
+        // Change emptyStringValue to 0 to revert back to Number()'s default behavior.
+        return x.length > 0 ? Number(x) : emptyStringValue;
       } else {
         return x;
       }
