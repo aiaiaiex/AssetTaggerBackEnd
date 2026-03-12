8ef95dd9742de4976ea6e2fabca7ba85157277be
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-12 14:47:28 +0800

    Add comment

--- src/constants/RegExpConstants.ts
+++ src/constants/RegExpConstants.ts
@@ -1,2 +1,2 @@
 export const NO_WHITESPACE = /^\S*$/;
-export const NO_LEADING_AND_TRAILING_WHITESPACE = /(^\S+(\s*\S+)*$|^$)/;
+export const NO_LEADING_AND_TRAILING_WHITESPACE = /(^\S+(\s*\S+)*$|^$)/; // ^$ only matches an empty string.
