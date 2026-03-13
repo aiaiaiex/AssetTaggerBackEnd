f0c678818ac025c23b097c43c8e3937a75bfa2b0
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-13 14:15:35 +0800

    Remove 'A' from array passed to z.enum()
    I used 'A' while testing EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA in Bruno but I forgot to remove it before committing and pushing.

--- src/constants/ZodConstants.ts
+++ src/constants/ZodConstants.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA = z
   .string()
   .toUpperCase() // Make it case-insensitive by converting input to uppercase.
-  .pipe(z.enum(["", "!", "NULL", "A"])) // Make sure that all strings passed to enum() are uppercase!
+  .pipe(z.enum(["", "!", "NULL"])) // Make sure that all strings passed to enum() are uppercase!
   .meta({
     description:
       "matches the following case-insensitive strings ['', '!', 'NULL']",
