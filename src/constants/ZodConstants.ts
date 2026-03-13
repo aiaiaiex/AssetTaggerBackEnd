7fcea92391b7cd1b5da085c73f7049fecb539097
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-13 14:35:05 +0800

    Add comment

--- src/constants/ZodConstants.ts
+++ src/constants/ZodConstants.ts
@@ -5,10 +5,11 @@ import {
   NO_WHITESPACE,
 } from "./RegExpConstants";
 
+// This schema returns input in UPPERCASE during a successful parse.
 export const EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA = z
   .string()
-  .toUpperCase() // Make it case-insensitive by converting input to uppercase.
-  .pipe(z.enum(["", "!", "NULL"])) // Make sure that all strings passed to enum() are uppercase!
+  .toUpperCase() // Make it case-insensitive by converting input to UPPERCASE.
+  .pipe(z.enum(["", "!", "NULL"])) // Make sure that all strings passed to enum() are UPPERCASE!
   .meta({
     description:
       "matches the following case-insensitive strings ['', '!', 'NULL']",
