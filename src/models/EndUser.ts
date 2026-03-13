85cc107546611f33c7cf2ccc31bfc7565141a0c6
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-13 09:56:52 +0800

    Use zodExclude

--- src/models/EndUser.ts
+++ src/models/EndUser.ts
@@ -1,33 +1,18 @@
 import z from "zod";
 
-import { zodNoWhitespace } from "../utils/zodUtils";
+import { zodExclude, zodNoWhitespace } from "../utils/zodUtils";
 
 export const EndUserSchema = z.object({
   EmployeeID: z.uuid({ version: "v4" }),
   EndUserID: z.uuid({ version: "v4" }),
-  EndUserName: zodNoWhitespace
-    .min(1)
-    .max(4000)
-    // Create an 'inverse' validation (i.e., validation fails when input matches schema).
-    .transform((input, ctx) => {
-      const parsedInput = z
-        .enum(["", "!", "NULL"])
-        // Make it case-insensitive by converting input to uppercase.
-        // Make sure that all strings passed to enum() are uppercase!
-        .safeParse(input.toUpperCase());
-
-      if (parsedInput.success) {
-        ctx.issues.push({
-          code: "custom",
-          input: input,
-          message:
-            "Invalid input: in excluded case-insensitive strings ('', '!', 'NULL')",
-        });
-        return z.NEVER;
-      }
-
-      return input;
-    }),
+  EndUserName: zodExclude(
+    zodNoWhitespace.min(1).max(4000),
+    z
+      .string()
+      .toUpperCase() // Make it case-insensitive by converting input to uppercase.
+      .pipe(z.enum(["", "!", "NULL"])), // Make sure that all strings passed to enum() are uppercase!
+    "Invalid input: in excluded case-insensitive strings ('', '!', 'NULL')",
+  ),
   EndUserPassword: z.string().max(4000).optional(), // Optional because it is not in the database.
   EndUserPasswordHash: z.hash("sha512"),
   EndUserRegisterDate: z.date(),
