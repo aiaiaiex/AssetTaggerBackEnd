66603b335cf13daf9887b226d228fbc6771df7c5
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-13 14:11:18 +0800

    Use EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA

--- src/models/EndUser.ts
+++ src/models/EndUser.ts
@@ -1,5 +1,6 @@
 import z from "zod";
 
+import { EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA } from "../constants/ZodConstants";
 import { zodExclude, zodNoWhitespace } from "../utils/zodUtils";
 
 export const EndUserSchema = z.object({
@@ -7,11 +8,7 @@ export const EndUserSchema = z.object({
   EndUserID: z.uuid({ version: "v4" }),
   EndUserName: zodExclude(
     zodNoWhitespace.min(1).max(4000),
-    z
-      .string()
-      .toUpperCase() // Make it case-insensitive by converting input to uppercase.
-      .pipe(z.enum(["", "!", "NULL"])), // Make sure that all strings passed to enum() are uppercase!
-    "Invalid input: in excluded case-insensitive strings ('', '!', 'NULL')",
+    EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA,
   ),
   EndUserPassword: z.string().max(4000).optional(), // Optional because it is not in the database.
   EndUserPasswordHash: z.hash("sha512"),
