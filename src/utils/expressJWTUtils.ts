ef71a64a2aec426b40c7486310a46b5593885d79
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-15 12:26:45 +0800

    Add optional parameter named 'optional' to expressJWTGetMiddleware (formerly a constant named as expressJWTMiddleware) and expressJWTGetPayload to allow tokens and payloads to be optional
    To allow routes to be protected and unprotected at the same time

--- src/utils/expressJWTUtils.ts
+++ src/utils/expressJWTUtils.ts
@@ -6,31 +6,53 @@ import authenticationConfig from "../configs/authenticationConfig";
 import { ExpressError } from "../middlewares/handleError";
 import { Authentication, AuthenticationSchema } from "../models/Authentication";
 
-export const expressJWTGetPayload = (auth?: JwtPayload): Authentication => {
-  const parsedPayload = AuthenticationSchema.safeParse(auth);
+export function expressJWTGetPayload<T extends boolean = false>(
+  auth: JwtPayload | undefined,
+  optional?: T,
+): T extends true ? Authentication | undefined : Authentication;
+export function expressJWTGetPayload(
+  auth: JwtPayload | undefined,
+  optional = false,
+): Authentication | undefined {
+  const parsedPayload = optional
+    ? AuthenticationSchema.optional().safeParse(auth)
+    : AuthenticationSchema.safeParse(auth);
 
   if (!parsedPayload.success) {
     throw new ExpressError(z.prettifyError(parsedPayload.error), 400);
   }
 
   return parsedPayload.data;
+}
+
+export const expressJWTGetMiddleware = (optional = false) => {
+  return expressjwt({
+    algorithms: [authenticationConfig.algorithms],
+    credentialsRequired: !optional,
+    getToken: (req: Request) => {
+      const accessToken: unknown =
+        req.cookies[authenticationConfig.cookieAccessTokenName];
+
+      const parsedAccessToken = optional
+        ? z
+            .jwt({ alg: authenticationConfig.algorithms })
+            .optional()
+            .safeParse(accessToken)
+        : z
+            .jwt({ alg: authenticationConfig.algorithms })
+            .safeParse(accessToken);
+
+      if (!parsedAccessToken.success) {
+        throw new ExpressError(
+          accessToken === undefined
+            ? "No token!"
+            : "Provided token is not a JWT!",
+          400,
+        );
+      }
+
+      return parsedAccessToken.data;
+    },
+    secret: authenticationConfig.secret,
+  });
 };
-
-export const expressJWTMiddleware = expressjwt({
-  algorithms: [authenticationConfig.algorithms],
-  getToken: (req: Request) => {
-    const accesToken: unknown =
-      req.cookies[authenticationConfig.cookieAccessTokenName];
-
-    const parsedAccessToken = z
-      .jwt({ alg: authenticationConfig.algorithms })
-      .safeParse(accesToken);
-
-    if (!parsedAccessToken.success) {
-      throw new ExpressError("No token!", 400);
-    }
-
-    return parsedAccessToken.data;
-  },
-  secret: authenticationConfig.secret,
-});
