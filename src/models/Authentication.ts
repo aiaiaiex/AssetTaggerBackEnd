dc26ca7ad1b82ed20833d3051241277e2bec8fa4
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-15 14:49:58 +0800

    Create CallingEndUserIP property

--- src/models/Authentication.ts
+++ src/models/Authentication.ts
@@ -4,6 +4,7 @@ import { EndUserSchema } from "./EndUser";
 
 export const AuthenticationSchema = z.object({
   CallingEndUserID: EndUserSchema.shape.EndUserID,
+  CallingEndUserIP: z.xor([z.ipv4(), z.ipv6()]).optional(),
 });
 
 export type Authentication = z.infer<typeof AuthenticationSchema>;
