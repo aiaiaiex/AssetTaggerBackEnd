de610091d063c6df4449ff882d784e8ca81d43b7
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-18 14:22:24 +0800

    Create ProductSet model

---
+++ src/models/ProductSet.ts
@@ -0,0 +1,13 @@
+import z from "zod";
+
+import { MSSQL_INT_SCHEMA } from "../constants/ZodConstants";
+import { ProductSchema } from "./Product";
+
+export const ProductSetSchema = z.object({
+  ParentProductID: ProductSchema.shape.ProductID,
+  ProductID: ProductSchema.shape.ProductID,
+  ProductInsertDate: z.date(),
+  ProductSetProductQuantity: MSSQL_INT_SCHEMA.min(1),
+});
+
+export type ProductSet = z.infer<typeof ProductSetSchema>;
