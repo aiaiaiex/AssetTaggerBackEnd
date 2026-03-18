aae58ebaee77a3794d8180edf87c8bbf6087af71
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-18 13:30:23 +0800

    Create Company model

---
+++ src/models/Company.ts
@@ -0,0 +1,27 @@
+import z from "zod";
+
+import {
+  EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA,
+  NO_LEADING_AND_TRAILING_WHITESPACE_SCHEMA,
+} from "../constants/ZodConstants";
+import { zodExclude } from "../utils/zodUtils";
+
+export const CompanySchema = z.object({
+  CompanyAddress: zodExclude(
+    NO_LEADING_AND_TRAILING_WHITESPACE_SCHEMA.min(1).max(850),
+    EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA,
+  ),
+  CompanyCode: zodExclude(
+    NO_LEADING_AND_TRAILING_WHITESPACE_SCHEMA.min(1).max(5),
+    EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA,
+  ),
+  CompanyID: z.uuid({ version: "v4" }),
+  CompanyInsertDate: z.date(),
+  CompanyName: zodExclude(
+    NO_LEADING_AND_TRAILING_WHITESPACE_SCHEMA.min(1).max(850),
+    EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA,
+  ),
+  ParentCompanyID: z.uuid({ version: "v4" }).nullable(),
+});
+
+export type Company = z.infer<typeof CompanySchema>;
