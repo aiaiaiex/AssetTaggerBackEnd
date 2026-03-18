ac116fdda5bcebe9e966e4fffde81e81ed338915
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-18 13:01:26 +0800

    Create Role model

---
+++ src/models/Role.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+import {
+  EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA,
+  NO_LEADING_AND_TRAILING_WHITESPACE_SCHEMA,
+} from "../constants/ZodConstants";
+import { zodExclude } from "../utils/zodUtils";
+
+export const RoleSchema = z.object({
+  RoleID: z.uuid({ version: "v4" }),
+  RoleInsertDate: z.date(),
+  RoleName: zodExclude(
+    NO_LEADING_AND_TRAILING_WHITESPACE_SCHEMA.min(1).max(850),
+    EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA,
+  ),
+});
+
+export type Role = z.infer<typeof RoleSchema>;
