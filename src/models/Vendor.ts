c788c4fdc216644c4dd493f6ac64d3eabae88699
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-18 12:59:28 +0800

    Create Vendor model

---
+++ src/models/Vendor.ts
@@ -0,0 +1,22 @@
+import z from "zod";
+
+import {
+  EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA,
+  NO_LEADING_AND_TRAILING_WHITESPACE_SCHEMA,
+} from "../constants/ZodConstants";
+import { zodExclude } from "../utils/zodUtils";
+
+export const VendorSchema = z.object({
+  VendorAddress: zodExclude(
+    NO_LEADING_AND_TRAILING_WHITESPACE_SCHEMA.min(1).max(850),
+    EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA,
+  ),
+  VendorID: z.uuid({ version: "v4" }),
+  VendorInsertDate: z.date(),
+  VendorName: zodExclude(
+    NO_LEADING_AND_TRAILING_WHITESPACE_SCHEMA.min(1).max(850),
+    EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA,
+  ),
+});
+
+export type Vendor = z.infer<typeof VendorSchema>;
