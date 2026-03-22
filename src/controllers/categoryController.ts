3f8229cecccb98fef0cf0dd364e13502244e9910
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-23 06:43:14 +0800

    Create categoryController

---
+++ src/controllers/categoryController.ts
@@ -0,0 +1,357 @@
+import { Response } from "express";
+import { Request as JWTRequest } from "express-jwt";
+import sql from "mssql";
+import z from "zod";
+
+import {
+  USP_CREATE_CATEGORY,
+  USP_DELETE_CATEGORY,
+  USP_READ_CATEGORY,
+  USP_UPDATE_CATEGORY,
+} from "../constants/StoredProcedureConstants";
+import {
+  TSQL_BIT_SCHEMA,
+  TSQL_INT_SCHEMA,
+} from "../constants/TSQLDataTypeConstants";
+import { ExpressError } from "../middlewares/handleError";
+import { Category, CategorySchema } from "../models/Category";
+import { Log } from "../models/Log";
+import { expressJWTGetPayload } from "../utils/expressJWTUtils";
+import { zodParseDate, zodParseNumber, zodQuery } from "../utils/zodUtils";
+import { usp_CreateLog } from "./logController";
+
+export const createCategory = async (req: JWTRequest, res: Response) => {
+  const { CallingEndUserID } = expressJWTGetPayload(req.auth);
+
+  const parsedBody = CategorySchema.omit({
+    CategoryID: true,
+    CategoryInsertDate: true,
+  }).safeParse(req.body);
+
+  if (!parsedBody.success) {
+    throw new ExpressError(z.prettifyError(parsedBody.error), 400);
+  }
+
+  const { CategoryName } = parsedBody.data;
+
+  const storedProcedureStart: Log["LogStoredProcedureStart"] = new Date();
+  let storedProcedureEnd: Log["LogStoredProcedureEnd"] | undefined;
+  let storedProcedureSuccess: Log["LogStoredProcedureSuccess"] = 1;
+
+  await req.app.locals.database
+    .request()
+    .input("CallingEndUserID", sql.UniqueIdentifier, CallingEndUserID)
+    .input("CategoryName", sql.NVarChar(850), CategoryName)
+    .execute<Category>(USP_CREATE_CATEGORY)
+    .then(({ recordset }) => {
+      const parsedRecordset = CategorySchema.array()
+        .length(1)
+        .safeParse(recordset);
+      if (!parsedRecordset.success) {
+        throw new ExpressError(z.prettifyError(parsedRecordset.error), 500);
+      }
+
+      res.json(parsedRecordset.data[0]);
+    })
+    .catch((error: unknown) => {
+      storedProcedureEnd = storedProcedureEnd ?? new Date();
+      storedProcedureSuccess = 0;
+
+      if (error instanceof Error) {
+        throw error;
+      }
+    })
+    .finally(async () => {
+      await usp_CreateLog(
+        req.app.locals.database,
+        CallingEndUserID,
+        req.ip ?? null,
+        storedProcedureStart,
+        storedProcedureEnd,
+        storedProcedureSuccess,
+        USP_CREATE_CATEGORY,
+        JSON.stringify({ CallingEndUserID, ...parsedBody.data }),
+      );
+    });
+};
+
+export const readCategory = async (req: JWTRequest, res: Response) => {
+  const { CallingEndUserID } = expressJWTGetPayload(req.auth);
+
+  const parsedParams = CategorySchema.pick({
+    CategoryID: true,
+  }).safeParse(req.params);
+
+  if (!parsedParams.success) {
+    throw new ExpressError(z.prettifyError(parsedParams.error), 400);
+  }
+
+  const { CategoryID } = parsedParams.data;
+
+  const storedProcedureStart: Log["LogStoredProcedureStart"] = new Date();
+  let storedProcedureEnd: Log["LogStoredProcedureEnd"] | undefined;
+  let storedProcedureSuccess: Log["LogStoredProcedureSuccess"] = 1;
+
+  await req.app.locals.database
+    .request()
+    .input("CallingEndUserID", sql.UniqueIdentifier, CallingEndUserID)
+    .input("CategoryID", sql.UniqueIdentifier, CategoryID)
+    .execute<Category>(USP_READ_CATEGORY)
+    .then(({ recordset }) => {
+      const parsedRecordset = CategorySchema.array()
+        .max(1)
+        .safeParse(recordset);
+
+      if (!parsedRecordset.success) {
+        throw new ExpressError(z.prettifyError(parsedRecordset.error), 500);
+      }
+
+      res.json(parsedRecordset.data[0]);
+    })
+    .catch((error: unknown) => {
+      storedProcedureEnd = storedProcedureEnd ?? new Date();
+      storedProcedureSuccess = 0;
+
+      if (error instanceof Error) {
+        throw error;
+      }
+    })
+    .finally(async () => {
+      await usp_CreateLog(
+        req.app.locals.database,
+        CallingEndUserID,
+        req.ip ?? null,
+        storedProcedureStart,
+        storedProcedureEnd,
+        storedProcedureSuccess,
+        USP_READ_CATEGORY,
+        JSON.stringify({ CallingEndUserID, ...parsedParams.data }),
+      );
+    });
+};
+
+export const readCategorys = async (req: JWTRequest, res: Response) => {
+  const { CallingEndUserID } = expressJWTGetPayload(req.auth);
+
+  const parsedQuery = CategorySchema.omit({
+    CategoryID: true,
+    CategoryInsertDate: true,
+  })
+    .extend({
+      CategoryName: zodQuery([CategorySchema.shape.CategoryName]).prefault(
+        null,
+      ),
+    })
+    .safeExtend({
+      FromCategoryInsertDate: zodQuery([
+        zodParseDate(CategorySchema.shape.CategoryInsertDate),
+      ]).prefault(null),
+      NewestRowsFirst: zodQuery([zodParseNumber(TSQL_BIT_SCHEMA)]).prefault(
+        null,
+      ),
+      RowsToReturn: zodQuery([zodParseNumber(TSQL_INT_SCHEMA.min(1))]).prefault(
+        null,
+      ),
+      RowsToSkip: zodQuery([zodParseNumber(TSQL_INT_SCHEMA.min(0))]).prefault(
+        null,
+      ),
+      ToCategoryInsertDate: zodQuery([
+        zodParseDate(CategorySchema.shape.CategoryInsertDate),
+      ]).prefault(null),
+    })
+    .safeParse(req.query);
+
+  if (!parsedQuery.success) {
+    throw new ExpressError(z.prettifyError(parsedQuery.error), 400);
+  }
+
+  const {
+    CategoryName,
+    FromCategoryInsertDate,
+    NewestRowsFirst,
+    RowsToReturn,
+    RowsToSkip,
+    ToCategoryInsertDate,
+  } = parsedQuery.data;
+
+  const storedProcedureStart: Log["LogStoredProcedureStart"] = new Date();
+  let storedProcedureEnd: Log["LogStoredProcedureEnd"] | undefined;
+  let storedProcedureSuccess: Log["LogStoredProcedureSuccess"] = 1;
+
+  await req.app.locals.database
+    .request()
+    .input("CallingEndUserID", sql.UniqueIdentifier, CallingEndUserID)
+    .input("CategoryName", sql.NVarChar(850), CategoryName)
+    .input(
+      "FromCategoryInsertDate",
+      sql.DateTimeOffset(3),
+      FromCategoryInsertDate,
+    )
+    .input("ToCategoryInsertDate", sql.DateTimeOffset(3), ToCategoryInsertDate)
+    .input("RowsToSkip", sql.Int, RowsToSkip)
+    .input("RowsToReturn", sql.Int, RowsToReturn)
+    .input("NewestRowsFirst", sql.Bit, NewestRowsFirst)
+    .execute<Category>(USP_READ_CATEGORY)
+    .then(({ recordset }) => {
+      const parsedRecordset = CategorySchema.array().safeParse(recordset);
+
+      if (!parsedRecordset.success) {
+        throw new ExpressError(z.prettifyError(parsedRecordset.error), 500);
+      }
+
+      res.json(parsedRecordset.data);
+    })
+    .catch((error: unknown) => {
+      storedProcedureEnd = storedProcedureEnd ?? new Date();
+      storedProcedureSuccess = 0;
+
+      if (error instanceof Error) {
+        throw error;
+      }
+    })
+    .finally(async () => {
+      await usp_CreateLog(
+        req.app.locals.database,
+        CallingEndUserID,
+        req.ip ?? null,
+        storedProcedureStart,
+        storedProcedureEnd,
+        storedProcedureSuccess,
+        USP_READ_CATEGORY,
+        JSON.stringify({ CallingEndUserID, ...parsedQuery.data }),
+      );
+    });
+};
+
+export const updateCategory = async (req: JWTRequest, res: Response) => {
+  const { CallingEndUserID } = expressJWTGetPayload(req.auth);
+
+  const parsedParams = CategorySchema.pick({
+    CategoryID: true,
+  }).safeParse(req.params);
+
+  if (!parsedParams.success) {
+    throw new ExpressError(z.prettifyError(parsedParams.error), 400);
+  }
+
+  const { CategoryID } = parsedParams.data;
+
+  const parsedBody = CategorySchema.omit({
+    CategoryID: true,
+    CategoryInsertDate: true,
+  })
+    .extend({
+      CategoryName: CategorySchema.shape.CategoryName.nullable().prefault(null),
+    })
+    .safeParse(req.body);
+
+  if (!parsedBody.success) {
+    throw new ExpressError(z.prettifyError(parsedBody.error), 400);
+  }
+
+  const { CategoryName } = parsedBody.data;
+
+  const storedProcedureStart: Log["LogStoredProcedureStart"] = new Date();
+  let storedProcedureEnd: Log["LogStoredProcedureEnd"] | undefined;
+  let storedProcedureSuccess: Log["LogStoredProcedureSuccess"] = 1;
+
+  await req.app.locals.database
+    .request()
+    .input("CallingEndUserID", sql.UniqueIdentifier, CallingEndUserID)
+    .input("CategoryID", sql.UniqueIdentifier, CategoryID)
+    .input("CategoryName", sql.NVarChar(850), CategoryName)
+    .execute<Category>(USP_UPDATE_CATEGORY)
+    .then(({ recordset }) => {
+      const parsedRecordset = CategorySchema.safeExtend({
+        OldCategoryName: CategorySchema.shape.CategoryName,
+      })
+        .array()
+        .max(1)
+        .safeParse(recordset);
+
+      if (!parsedRecordset.success) {
+        throw new ExpressError(z.prettifyError(parsedRecordset.error), 500);
+      }
+
+      res.json(parsedRecordset.data[0]);
+    })
+    .catch((error: unknown) => {
+      storedProcedureEnd = storedProcedureEnd ?? new Date();
+      storedProcedureSuccess = 0;
+
+      if (error instanceof Error) {
+        throw error;
+      }
+    })
+    .finally(async () => {
+      await usp_CreateLog(
+        req.app.locals.database,
+        CallingEndUserID,
+        req.ip ?? null,
+        storedProcedureStart,
+        storedProcedureEnd,
+        storedProcedureSuccess,
+        USP_UPDATE_CATEGORY,
+        JSON.stringify({
+          CallingEndUserID,
+          ...parsedParams.data,
+          ...parsedBody.data,
+        }),
+      );
+    });
+};
+
+export const deleteCategory = async (req: JWTRequest, res: Response) => {
+  const { CallingEndUserID } = expressJWTGetPayload(req.auth);
+
+  const parsedParams = CategorySchema.pick({
+    CategoryID: true,
+  }).safeParse(req.params);
+
+  if (!parsedParams.success) {
+    throw new ExpressError(z.prettifyError(parsedParams.error), 400);
+  }
+
+  const { CategoryID } = parsedParams.data;
+
+  const storedProcedureStart: Log["LogStoredProcedureStart"] = new Date();
+  let storedProcedureEnd: Log["LogStoredProcedureEnd"] | undefined;
+  let storedProcedureSuccess: Log["LogStoredProcedureSuccess"] = 1;
+
+  await req.app.locals.database
+    .request()
+    .input("CallingEndUserID", sql.UniqueIdentifier, CallingEndUserID)
+    .input("CategoryID", sql.UniqueIdentifier, CategoryID)
+    .execute<Category>(USP_DELETE_CATEGORY)
+    .then(({ recordset }) => {
+      const parsedRecordset = CategorySchema.array()
+        .max(1)
+        .safeParse(recordset);
+
+      if (!parsedRecordset.success) {
+        throw new ExpressError(z.prettifyError(parsedRecordset.error), 500);
+      }
+
+      res.json(parsedRecordset.data[0]);
+    })
+    .catch((error: unknown) => {
+      storedProcedureEnd = storedProcedureEnd ?? new Date();
+      storedProcedureSuccess = 0;
+
+      if (error instanceof Error) {
+        throw error;
+      }
+    })
+    .finally(async () => {
+      await usp_CreateLog(
+        req.app.locals.database,
+        CallingEndUserID,
+        req.ip ?? null,
+        storedProcedureStart,
+        storedProcedureEnd,
+        storedProcedureSuccess,
+        USP_DELETE_CATEGORY,
+        JSON.stringify({ CallingEndUserID, ...parsedParams.data }),
+      );
+    });
+};
