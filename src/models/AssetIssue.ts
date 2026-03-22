94bfafba22c65c9d2b66c3a83e6ab63695e9934c
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-22 22:33:30 +0800

    Create AssetIssue model

---
+++ src/models/AssetIssue.ts
@@ -0,0 +1,37 @@
+import z from "zod";
+
+import {
+  EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA,
+  EXCLUDED_DATETIMEOFFSET_SCHEMA,
+  EXCLUDED_UNIQUEIDENTIFIER_SCHEMA,
+  NO_LEADING_AND_TRAILING_WHITESPACE_SCHEMA,
+  NORMALIZED_WEB_URL_SCHEMA,
+} from "../constants/CheckConstraintConstants";
+import { TSQL_DATETIMEOFFSET_SCHEMA } from "../constants/TSQLDataTypeConstants";
+import { zodExclude } from "../utils/zodUtils";
+import { AssetSchema } from "./Asset";
+import { EmployeeSchema } from "./Employee";
+
+export const AssetIssueSchema = z.object({
+  AssetID: AssetSchema.shape.AssetID,
+  AssetIssueDate: zodExclude(
+    TSQL_DATETIMEOFFSET_SCHEMA,
+    EXCLUDED_DATETIMEOFFSET_SCHEMA,
+  ),
+  AssetIssueDescription: zodExclude(
+    NO_LEADING_AND_TRAILING_WHITESPACE_SCHEMA.min(1),
+    EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA,
+  ).nullable(),
+  AssetIssueDocumentationURL: NORMALIZED_WEB_URL_SCHEMA.nullable(),
+  AssetIssueID: zodExclude(
+    z.uuid({ version: "v4" }),
+    EXCLUDED_UNIQUEIDENTIFIER_SCHEMA,
+  ),
+  AssetIssueTitle: zodExclude(
+    NO_LEADING_AND_TRAILING_WHITESPACE_SCHEMA.min(1).max(4000),
+    EXCLUDED_CASE_INSENSITIVE_NVARCHAR_SCHEMA,
+  ),
+  EmployeeID: EmployeeSchema.shape.EmployeeID,
+});
+
+export type AssetIssue = z.infer<typeof AssetIssueSchema>;
