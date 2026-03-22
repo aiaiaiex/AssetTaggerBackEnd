80403fd6d0e0b86830f236b6fa4045132602f87d
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-22 18:41:21 +0800

    Make ServerName not be an empty string

--- src/models/DatabaseServer.ts
+++ src/models/DatabaseServer.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const DatabaseServerSchema = z.object({
   // https://learn.microsoft.com/en-us/sql/t-sql/functions/servername-transact-sql
-  ServerName: z.string(),
+  ServerName: z.string().min(1),
 });
 
 export type DatabaseServer = z.infer<typeof DatabaseServerSchema>;
