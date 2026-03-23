161e581c7fee95f5846edcd3718a1e774f10dd3e
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-23 14:49:08 +0800

    Use Create departmentRoutes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { expressJWTGetMiddleware } from "../utils/expressJWTUtils";
 import authenticationRoutes from "./authenticationRoutes";
+import departmentRoutes from "./departmentRoutes";
 import endUserRoleRoutes from "./endUserRoleRoutes";
 import endUserRoutes from "./endUserRoutes";
 import logRoutes from "./logRoutes";
@@ -16,6 +17,7 @@ routes.use("/", rootRoutes);
 routes.use("/authentication", authenticationRoutes);
 routes.use("/enduser", expressJWTGetMiddleware(), endUserRoutes);
 routes.use("/log", expressJWTGetMiddleware(), logRoutes);
+routes.use("/department", expressJWTGetMiddleware(), departmentRoutes);
 routes.use("/enduserrole", expressJWTGetMiddleware(), endUserRoleRoutes);
 routes.use("/manufacturer", expressJWTGetMiddleware(), manufacturerRoutes);
 routes.use("/role", expressJWTGetMiddleware(), roleRoutes);
