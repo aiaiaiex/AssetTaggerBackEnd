2c1891e38e40c732eabaf0dc05c8d6ce32cc7839
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-23 14:42:55 +0800

    Use vendorRoutes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,6 +5,7 @@ import authenticationRoutes from "./authenticationRoutes";
 import endUserRoutes from "./endUserRoutes";
 import logRoutes from "./logRoutes";
 import rootRoutes from "./rootRoutes";
+import vendorRoutes from "./vendorRoutes";
 
 const routes = Router();
 
@@ -12,5 +13,6 @@ routes.use("/", rootRoutes);
 routes.use("/authentication", authenticationRoutes);
 routes.use("/enduser", expressJWTGetMiddleware(), endUserRoutes);
 routes.use("/log", expressJWTGetMiddleware(), logRoutes);
+routes.use("/vendor", expressJWTGetMiddleware(), vendorRoutes);
 
 export default routes;
