41bcafab542180dbe3868fd6067a85bac56b7544
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-23 14:54:53 +0800

    Use buildingRoutes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { expressJWTGetMiddleware } from "../utils/expressJWTUtils";
 import authenticationRoutes from "./authenticationRoutes";
+import buildingRoutes from "./buildingRoutes";
 import categoryRoutes from "./categoryRoutes";
 import companyRoutes from "./companyRoutes";
 import departmentRoutes from "./departmentRoutes";
@@ -21,6 +22,7 @@ routes.use("/", rootRoutes);
 routes.use("/authentication", authenticationRoutes);
 routes.use("/enduser", expressJWTGetMiddleware(), endUserRoutes);
 routes.use("/log", expressJWTGetMiddleware(), logRoutes);
+routes.use("/building", expressJWTGetMiddleware(), buildingRoutes);
 routes.use("/employee", expressJWTGetMiddleware(), employeeRoutes);
 routes.use("/product", expressJWTGetMiddleware(), productRoutes);
 routes.use("/category", expressJWTGetMiddleware(), categoryRoutes);
