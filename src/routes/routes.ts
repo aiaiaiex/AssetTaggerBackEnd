d1d88e18743efdc45cd3d0e4dc312d80bbef1745
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-23 14:45:42 +0800

    Use manufacturerRoutes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,7 @@ import { expressJWTGetMiddleware } from "../utils/expressJWTUtils";
 import authenticationRoutes from "./authenticationRoutes";
 import endUserRoutes from "./endUserRoutes";
 import logRoutes from "./logRoutes";
+import manufacturerRoutes from "./manufacturerRoutes";
 import roleRoutes from "./roleRoutes";
 import rootRoutes from "./rootRoutes";
 import vendorRoutes from "./vendorRoutes";
@@ -14,6 +15,7 @@ routes.use("/", rootRoutes);
 routes.use("/authentication", authenticationRoutes);
 routes.use("/enduser", expressJWTGetMiddleware(), endUserRoutes);
 routes.use("/log", expressJWTGetMiddleware(), logRoutes);
+routes.use("/manufacturer", expressJWTGetMiddleware(), manufacturerRoutes);
 routes.use("/role", expressJWTGetMiddleware(), roleRoutes);
 routes.use("/vendor", expressJWTGetMiddleware(), vendorRoutes);
 
